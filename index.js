f98e76a95bdeea5bf318cb5dae8b4b644d67d141
MagicPojska <pojskicsafet@gmail.com>
2021-10-25 20:47:59 +0200

    Started my server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import express from "express";
+import axios from "axios";
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/", (req, res) => {
+  res.send("Welcome to my footbal news API");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
