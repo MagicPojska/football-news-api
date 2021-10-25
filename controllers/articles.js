a847a666da69381117902f860e53638e9c3602e9
MagicPojska <pojskicsafet@gmail.com>
2021-10-25 20:54:50 +0200

    Addec basic file structuring to my project. That means I added controllers and routes folders

---
+++ controllers/articles.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseUrl = "";
+
+//Get news articles
+export const getArticles = async (req, res) => {
+  res.send("Hello");
+};
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
-import axios from "axios";
+import articleRoute from "./routes/articles.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.send("Welcome to my footbal news API");
 });
+
+app.use("/article", articleRoute);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
